def6d56b49527ebed3fe089e0123eec22eec6d1c
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-11 20:19:10 +0300

    App models

--- department/models.py
+++ department/models.py
@@ -12,25 +12,26 @@ class Product(models.Model):
     author = models.ForeignKey('auth.User')
     name = models.CharField(max_length=50)
     detail = models.TextField()
-    type_of_product = models.CharField(max_length=2, choice=TYPE_OF_PERODUCT)
+    type_of_product = models.CharField(max_length=2, choices=TYPE_OF_PERODUCT)
     pub_date = models.DateTimeField(default=timezone.now)
-    photo = models.ImageField(upload_to='image')
+    photo = models.ImageField(upload_to='images', blank=True)
     priсe = models.DecimalField(max_digits=9, decimal_places=2)
-    in_basket = models.BooleanField(default=False)
     like = models.PositiveIntegerField(default=0)
     dislike = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
+class Seller(models.Model):
+    pub_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=2, decimal_places=2)
+    phone_number = models.PositiveIntegerField(blank=True)
+
 
 class User(models.Model):
-    desire = models.ForeignKey(Product,  on_delete=models.CASCADE)
     ban = models.BooleanField(default=False)
+    desire = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    seller = models.OneToOneField(Seller, on_delete=models.CASCADE, blank=True)
 
     def cost_of_desires(self):
         pass
-
-
-class Seller(models.Model):
-    #name and date 
-    pub_product = models.Foreignkey(Product, on_delete=models.CASCADE)
-    rating = models.DecimalField(max_digits=2, max_length=2)
-    #phone_number = models.PositiveIntegerField()
