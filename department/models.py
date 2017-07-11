c190976692800eb7ab0e2161e8c2d6501c4011b9
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-11 18:13:53 +0300

    Add  Seller model

--- department/models.py
+++ department/models.py
@@ -3,10 +3,18 @@ from django.utils import timezone
 
 
 class Product(models.Model):
+    TYPE_OF_PERODUCT = (
+        ('MB', 'Mobile'),
+        ('LP', 'Laptop'),
+        ('TB', 'Tablet'),
+        ('CN', 'Consoles'),
+    )
     author = models.ForeignKey('auth.User')
     name = models.CharField(max_length=50)
     detail = models.TextField()
+    type_of_product = models.CharField(max_length=2, choice=TYPE_OF_PERODUCT)
     pub_date = models.DateTimeField(default=timezone.now)
+    photo = models.ImageField(upload_to='image')
     priсe = models.DecimalField(max_digits=9, decimal_places=2)
     in_basket = models.BooleanField(default=False)
     like = models.PositiveIntegerField(default=0)
@@ -14,7 +22,15 @@ class Product(models.Model):
 
 
 class User(models.Model):
-    desire = models.ForeignKey(Product)
+    desire = models.ForeignKey(Product,  on_delete=models.CASCADE)
     ban = models.BooleanField(default=False)
 
-#add def Sub-market prices
+    def cost_of_desires(self):
+        pass
+
+
+class Seller(models.Model):
+    #name and date 
+    pub_product = models.Foreignkey(Product, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=2, max_length=2)
+    #phone_number = models.PositiveIntegerField()
