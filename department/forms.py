6d3de8f914e3a4a9a86564820e67e7b45d2b31cb
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-07-12 15:53:47 +0300

    Add part reg and log

---
+++ department/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+
+from betterforms.multiform import MultiModelForm
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
--- department/views.py
+++ department/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+
+class RegistrationView(CreateView):
+    pass
+
+
+def main(request):
+    return render(request, 'main.html')
+
 
 # Create your views here.
