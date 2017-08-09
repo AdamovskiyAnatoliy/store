eeaf3f7ccf1997ce4814c5ce8c3c5059204d6b5b
AdamovskiyAnatoliy <adamovskiy.job@gmail.com>
2017-08-09 23:24:21 +0300

    Add product form

--- department/forms.py
+++ department/forms.py
@@ -3,3 +3,14 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 
 from betterforms.multiform import MultiModelForm
+
+from .models import Product
+
+class ProductForm(ModelForm):
+
+    class Meta:
+        model = Product
+        fields = (
+            'name_product', 'detail', 'type_of_product', 'pub_date',
+            'photo',
+        )
